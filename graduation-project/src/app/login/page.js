b2d523e99e5f8141d4bee399451ed6184e36415a
Maher Nassrallah <maher.ple2@gmail.com>
2023-03-24 16:59:09 +0200

    login beta fetching

--- graduation-project/src/app/login/page.js
+++ graduation-project/src/app/login/page.js
@@ -1,54 +1,117 @@
+"use client";
 import Card from "@/components/Card";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 function Login() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const fetchDataHandler = useCallback(async () => {
+    try {
+      const response = await fetch(
+        "https://react-http-fa04a-default-rtdb.firebaseio.com/users/user1.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await response.json();
+      setEmail(data.email);
+      setPassword(data.password);
+    } catch (error) {
+      setError(error.message);
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchDataHandler(); //empty data when fetched at the beggining ?? 
+  }, []);
+
+
+  const onSubmit = async (data, e) => {
+    e.preventDefault();
+    fetchDataHandler();
+
+    if (email == data.email && password == data.password) {
+      console.log(email + "\n" + password);
+    } else console.log("error");
+  };
+
+  const onError = () => {
+    console.log("error");
+  };
+
+  //       console.log(data);
+  //       // localStorage.setItem("Token", res.data.accessToken);
+  //       // if (res.statusCode >= 400) setError("user not found");
+  //       // else if (res.statusCode < 400) {
+  //       //   console.log("LOGGEDIN");
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
         <div className="text-center">
           <h1 className="text-2xl font-semibold text-gray-700">LeapStart</h1>
           <h1 className="text-2xl font-semibold mt-5 mb-4 text-gray-700">
             Log in to your account
           </h1>
         </div>
-          <div className="mb-6">
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-semibold text-gray-900"
-            >
-              Email address
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:outline-none focus:border-gray-700 block w-full p-2"
-              placeholder="Enter your email address"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="password"
-              className="block mb-2 text-sm font-semibold text-gray-900"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:outline-none focus:border-gray-700 block w-full p-2"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-semibold text-gray-900"
+          >
+            Email address
+          </label>
+          <input
+            {...register("email", { required: true })}
+            type="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:outline-none focus:border-gray-700 block w-full p-2"
+            placeholder="Enter your email address"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-semibold text-gray-900"
+          >
+            Password
+          </label>
+          <input
+            {...register("password", { required: true })}
+            type="password"
+            id="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:outline-none focus:border-gray-700 block w-full p-2"
+            placeholder="Enter your password"
+            required
+          />
+        </div>
         <button
-          type="button"
+          type="submit"
           className="text-white dark:bg-indigo-500 bg-indigo-500 h-8 w-full hover:bg-indigo-700 focus:outline-none font-bold rounded text-sm px-5 py-1 mt-2 mr-2 mb-1"
         >
           Log in
         </button>
 
-        <Link className="text-[#2D65E4] font-semibold text-sm self-end object-right ml-52" href="/forgotPassword">
+        <Link
+          className="text-[#2D65E4] font-semibold text-sm self-end object-right ml-52"
+          href="/forgotPassword"
+        >
           Forgot password?
         </Link>
 
@@ -81,14 +144,14 @@ function Login() {
           Sign in with Google
         </button>
         <div className="text-center">
-        <hr className="h-px mt-2 mb-3 -mx-14 bg-gray-200 border-0 dark:bg-gray-300" />
+          <hr className="h-px mt-2 mb-3 -mx-14 bg-gray-200 border-0 dark:bg-gray-300" />
 
-        <Link
-          className="text-[#2D65E4] font-bold text-center text-sm mt-10"
-          href="/SignUp"
-        >
-          Don&#39;t have an account? Sign up
-        </Link>
+          <Link
+            className="text-[#2D65E4] font-bold text-center text-sm mt-10"
+            href="/SignUp"
+          >
+            Don&#39;t have an account? Sign up
+          </Link>
         </div>
       </form>
     </Card>
