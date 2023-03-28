4de44e8eaa7c84cd2bfddbf46d22e68fec8154d4
Maher Nassrallah <maher.ple2@gmail.com>
2023-03-28 15:32:34 +0300

    pull

--- graduation-project/src/app/auth/login/page.js
+++ graduation-project/src/app/auth/login/page.js
@@ -170,3 +170,5 @@ function Login() {
 }
 
 export default Login;
+
+SWR
--- graduation-project/src/components/Card.js
+++ graduation-project/src/components/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const Card = (props) => {
+  console.log(props.width);
   return (
-    <div className={`w-[${props.width}px] bg-white m-auto mt-[70px] mb-[70px] rounded shadow-lg px-14 py-8`}>{props.children}</div>
+    <div className={`!w-[430px] bg-white m-auto mt-[70px] mb-[70px] rounded shadow-lg px-14 py-8`}>{props.children}</div>
   );
 };
 
